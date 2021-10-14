fb792af9f743fe5a31325aaccec1541aec77dcc2
asw22wsa <siudevelop@gmail.com>
2021-10-14 09:35:40 +0900

    first commit

---
+++ client/src/Config.js
@@ -0,0 +1,7 @@
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+export const API_KEY = '15153dbe095176311b9aa8fa691f9ce6';
